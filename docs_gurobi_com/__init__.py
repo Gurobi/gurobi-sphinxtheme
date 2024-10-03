0604345533bcb1f9a7fe79dc4d70e3401be0156f
Simon Bowly <bowly@gurobi.com>
2024-10-03 14:15:20 +1000

    [#10] Add common config options

--- docs_gurobi_com/__init__.py
+++ docs_gurobi_com/__init__.py
@@ -6,5 +6,19 @@ logger = logging.getLogger(__name__)
 here = pathlib.Path(__file__).parent
 
 
+def update_config(config):
+    # Update configuration with some common properties used across
+    # docs.gurobi.com. Note that this overrides settings from individual
+    # project's conf.py files.
+    config.copyright = "2024, Gurobi Optimization, LLC"
+    config.author = "Gurobi Optimization, LLC"
+    config.html_favicon = "https://www.gurobi.com/favicon.ico"
+
+
+def builder_inited(app):
+    update_config(app.config)
+
+
 def setup(app):
     app.add_html_theme("docs_gurobi_com", here / "theme")
+    app.connect("builder-inited", builder_inited)
