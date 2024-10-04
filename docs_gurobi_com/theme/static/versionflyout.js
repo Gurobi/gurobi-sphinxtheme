12d06371e7486d657f96f910c93e40a88e123866
Simon Bowly <bowly@gurobi.com>
2024-10-04 13:20:23 +1000

    Fix light/dark behaviour
    - Icons need to be in the HTML, not CSS, to pick up the CSS
      variable changes for light/dark mode

--- docs_gurobi_com/theme/static/versionflyout.js
+++ docs_gurobi_com/theme/static/versionflyout.js
@@ -130,9 +130,16 @@ document.addEventListener("readthedocs-addons-data-ready", function (event) {
   const flyout = `
     <div class="rst-versions" data-toggle="rst-versions" role="note">
       <span class="rst-current-version" data-toggle="rst-current-version">
-        <span class="fa fa-book"> Gurobi</span>
+        <span class="fa fa-book">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -100 1664 1664" width="13" height="13" style="transform: scaleY(-1) translateY(-1px);">
+            <path fill="var(--color-foreground-primary)" d="M1639 1058q40 -57 18 -129l-275 -906q-19 -64 -76.5 -107.5t-122.5 -43.5h-923q-77 0 -148.5 53.5t-99.5 131.5q-24 67 -2 127q0 4 3 27t4 37q1 8 -3 21.5t-3 19.5q2 11 8 21t16.5 23.5t16.5 23.5q23 38 45 91.5t30 91.5q3 10 0.5 30t-0.5 28q3 11 17 28t17 23q21 36 42 92t25 90q1 9 -2.5 32t0.5 28q4 13 22 30.5t22 22.5q19 26 42.5 84.5t27.5 96.5q1 8 -3 25.5t-2 26.5q2 8 9 18t18 23t17 21q8 12 16.5 30.5t15 35t16 36t19.5 32t26.5 23.5t36 11.5t47.5 -5.5l-1 -3q38 9 51 9h761q74 0 114 -56t18 -130l-274 -906q-36 -119 -71.5 -153.5t-128.5 -34.5h-869q-27 0 -38 -15q-11 -16 -1 -43q24 -70 144 -70h923q29 0 56 15.5t35 41.5l300 987q7 22 5 57q38 -15 59 -43zM575 1056q-4 -13 2 -22.5t20 -9.5h608q13 0 25.5 9.5t16.5 22.5l21 64q4 13 -2 22.5t-20 9.5h-608q-13 0 -25.5 -9.5t-16.5 -22.5zM492 800q-4 -13 2 -22.5t20 -9.5h608q13 0 25.5 9.5t16.5 22.5l21 64q4 13 -2 22.5t-20 9.5h-608q-13 0 -25.5 -9.5t-16.5 -22.5z" />
+          </svg>
+          Gurobi
+        </span>
         ${thisVersionName}
-        <span class="fa fa-caret-up"></span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24">
+          <path fill="var(--color-foreground-primary)" d="M12 5.83L15.17 9 16.59 7.59 12 3 7.41 7.59 8.83 9 12 5.83zM12 18.17L8.83 15 7.41 16.41 12 21l4.59-4.59L15.17 15 12 18.17z"/>
+        </svg>
       </span>
       <div class="rst-other-versions">
         <div class="injected">
@@ -162,16 +169,8 @@ document.addEventListener("readthedocs-addons-data-ready", function (event) {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
-      var spanElement = document.querySelector('span.fa-caret-down');
-      if (spanElement) {
-        spanElement.classList.replace('fa-caret-down', 'fa-caret-up');
-      }
       content.style.display = "none";
     } else {
-      var spanElement = document.querySelector('span.fa-caret-up');
-      if (spanElement) {
-        spanElement.classList.replace('fa-caret-up', 'fa-caret-down');
-      }
       content.style.display = "block";
     }
   });
