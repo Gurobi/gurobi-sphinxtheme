49777576c6f666a392d6faa6c79ef78fc45c2c8d
Simon Bowly <bowly@gurobi.com>
2025-11-11 15:44:21 +1100

    Remove beta labelling for 13.0

--- docs_gurobi_com/versions.py
+++ docs_gurobi_com/versions.py
@@ -2,11 +2,11 @@ class VersionHandler:
 
     # String values or None
     GUROBI_CURRENT_RELEASE = "12.0"
-    GUROBI_BETA_RELEASE = "13.0"
+    GUROBI_BETA_RELEASE = None
 
     # Numeric values
     CURRENT_RELEASE_MIN = 12.0
-    CURRENT_RELEASE_MAX = 12.0
+    CURRENT_RELEASE_MAX = 13.0
 
     def is_released_version(self, version):
         """Expects a version label from conf.py"""
